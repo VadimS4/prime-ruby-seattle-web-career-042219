678125613a0004a962db38be75b9ba8bd2acb8fc
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-11 08:03:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,9 @@ def prime?(number)
   false if number < 2
   
   (2..num).each do |factor|
-    if (num % factor).zero?
-      return false
-      
+      if (num % factor).zero?
+        return false
+      end
+    end
+  true
 end
